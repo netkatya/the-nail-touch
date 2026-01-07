fafef352c3a93c98c371288955cb31d673f33b50
Ievgen Prygoda <e.klimochenkova@gmail.com>
2026-01-07 12:04:39 +0000

    init+header background+hero background

---
+++ components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-(--color-accent-dark) h-20">
+      <div className="container"></div>
+    </header>
+  );
+}
---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+  return (
+    <section>
+      <div className="container">
+        <div className="w-full h-200 bg-[url('/img/hero.webp')] bg-cover bg-center"></div>
+      </div>
+    </section>
+  );
+}

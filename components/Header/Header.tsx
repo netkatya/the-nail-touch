bb8341075132b9ae0191471fa2613ac271ced8df
Ievgen Prygoda <e.klimochenkova@gmail.com>
2026-01-07 13:13:33 +0000

    logo added

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <header className="bg-(--color-accent-dark) h-20">
-      <div className="container"></div>
+    <header className="bg-(--color-accent-dark) h-25 py-2.5">
+      <div className="container">
+        <div>
+          <Link href="/">
+            <Image
+              src="/img/logo-lighttheme.png"
+              alt="Logo"
+              width={80}
+              height={80}
+            ></Image>
+          </Link>
+        </div>
+      </div>
     </header>
   );
 }
